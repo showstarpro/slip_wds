71b4305078a92782ccdedc140ce68c2db3727a56
showstarpro <1359947339@qq.com>
2024-12-07 12:46:17 +0000

    feat: add two losses

--- losses.py
+++ losses.py
@@ -6,6 +6,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch import distributed as dist
 
 import utils
 
@@ -17,7 +18,7 @@ class CLIPLoss(nn.Module):
         self.last_local_batch_size = None
 
     def forward(self, outputs):
-        image_embed = outputs['image_embed']
+        image_embed = outputs['img_embed_p']
         text_embed = outputs['text_embed']
         logit_scale = outputs['logit_scale']
         local_batch_size = image_embed.size(0)
@@ -130,3 +131,93 @@ class SLIPLoss(nn.Module):
                 'clip_acc': clip_acc,
                 'ssl_loss': ssl_loss,
                 'ssl_acc': ssl_acc}
+
+def mean_flat(tensor):
+    """
+    Take the mean over all non-batch dimensions.
+    """
+    return tensor.mean(dim=list(range(1, len(tensor.shape))))
+
+class DiffLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, outputs):
+        rec_noise = outputs['rec_noise']
+        noise = outputs['noise']
+        ## L2 loss
+        loss = mean_flat((noise - rec_noise) ** 2).mean()
+
+        return {'loss':loss, 'diff_loss':loss}
+
+class CLSLoss(nn.Module):
+    def __init__(
+            self,
+            world_size,
+            pad_id=0,  # pad_token for open_clip custom tokenizer
+        ):
+        super().__init__()
+        self.pad_id = pad_id
+        self.world_size = world_size
+
+    def loss(self, logits, targets):
+        norm_item = F.normalize(targets, p=1, dim=1)
+        loss =  -(F.log_softmax(logits, dim=1) * norm_item).sum(dim=1).mean()
+        return loss
+    
+    def reweight_targets(self, cap_fq, num_samples, targets):
+        cap_fq += targets.sum(dim=0, keepdim=True) / targets.shape[0]
+        num_samples += 1
+        dist.all_reduce(cap_fq, op=dist.ReduceOp.AVG)
+        dist.all_reduce(num_samples, op=dist.ReduceOp.AVG)
+        all_batch_size = self.world_size * targets.shape[0]
+        targets = targets * torch.log((num_samples+1.0/all_batch_size) / (cap_fq+1.0/all_batch_size)).to(dtype=targets.dtype)
+        return targets
+
+    def forward(self, outputs):
+        labels = outputs['labels']
+        text_tokens = outputs['text_tokens']
+        cap_fq = outputs['cap_fq']
+        num_samples = outputs['num_samples']
+        B, C = text_tokens.shape
+        targets = torch.zeros(B, C, dtype=torch.float32).to(labels.device)
+        # scatter labels to one-hot
+        targets.scatter_(dim=1, index=labels, value=1.0)
+        targets = self.reweight_targets(cap_fq, num_samples, targets)
+        loss = self.loss(text_tokens, targets)
+
+        return {'loss': loss, 'cls_loss': loss}
+
+class MultiTaskLoss(nn.Module):
+    def __init__(self, ssl_loss, ssl_scale, diff_scale, cls_scale):
+        super().__init__()
+        self.clip_loss = CLIPLoss()
+        self.ssl_loss = ssl_loss
+        self.ssl_scale = ssl_scale
+        self.diff_loss = DiffLoss()
+        self.diff_scale = diff_scale
+        self.cls_loss = CLSLoss(world_size=1)
+        self.cls_scale = cls_scale
+
+    def forward(self, outputs):
+        clip_loss_dict = self.clip_loss(outputs)
+        clip_loss = clip_loss_dict['clip_loss']
+        clip_acc = clip_loss_dict['clip_acc']
+
+        ssl_loss_dict = self.ssl_loss(outputs)
+        ssl_loss = ssl_loss_dict['ssl_loss']
+        ssl_acc = ssl_loss_dict['ssl_acc']
+
+        diff_loss_dict = self.diff_loss(outputs)
+        diff_loss = diff_loss_dict['diff_loss']
+
+        cls_loss_dict = self.cls_loss(outputs)
+        cls_loss = cls_loss_dict['cls_loss']
+
+        return {'loss': clip_loss + self.ssl_scale * ssl_loss + self.diff_scale * diff_loss + self.cls_scale * cls_loss,
+                'clip_loss': clip_loss,
+                'clip_acc': clip_acc,
+                'ssl_loss': ssl_loss,
+                'ssl_acc': ssl_acc,
+                'diff_loss': diff_loss,
+                'cls_loss': cls_loss}
--- models.py
+++ models.py
@@ -6,15 +6,26 @@
 
 # Modified from github.com/openai/CLIP
 from collections import OrderedDict
-
+from typing import Optional
 import numpy as np
 import timm
 import torch
 from torch import nn
+import math
+from torch.utils.checkpoint import checkpoint
 from transformers import CLIPConfig, CLIPModel
-
+from dataclasses import dataclass
 import losses
 from auxilary import MultiheadAttention
+from diffusion import create_diffusion, gaussian_diffusion
+from cls_model.transformer import (
+    LayerNormFp32,
+    LayerNorm,
+    QuickGELU,
+    MultimodalTransformer,
+    MixClsHead,
+)
+from cls_model.model import CLIPTextCfg
 
 class LayerNorm(nn.LayerNorm):
     """Subclass torch's LayerNorm to handle fp16."""
@@ -252,7 +263,348 @@ class SLIP(CLIP):
                 'aug1_embed': aug1_embed,
                 'aug2_embed': aug2_embed}
 
+class TimestepEmbedder(nn.Module):
+    """
+    Embeds scalar timesteps into vector representations.
+    """
+    def __init__(self, hidden_size, frequency_embedding_size=256):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+        self.frequency_embedding_size = frequency_embedding_size
 
+    @staticmethod
+    def timestep_embedding(t, dim, max_period=10000):
+        """
+        Create sinusoidal timestep embeddings.
+        :param t: a 1-D Tensor of N indices, one per batch element.
+                          These may be fractional.
+        :param dim: the dimension of the output.
+        :param max_period: controls the minimum frequency of the embeddings.
+        :return: an (N, D) Tensor of positional embeddings.
+        """
+        # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
+        half = dim // 2
+        freqs = torch.exp(
+            -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
+        ).to(device=t.device)
+        args = t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+        if dim % 2:
+            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+        return embedding
+
+    def forward(self, t):
+        t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
+        t_emb = self.mlp(t_freq)
+        return t_emb
+
+def modulate(x, shift, scale):
+    return x * (1 + scale) + shift
+
+class ResBlock(nn.Module):
+    """
+    A residual block that can optionally change the number of channels.
+    :param channels: the number of input channels.
+    """
+
+    def __init__(
+        self,
+        channels
+    ):
+        super().__init__()
+        self.channels = channels
+
+        self.in_ln = nn.LayerNorm(channels, eps=1e-6)
+        self.mlp = nn.Sequential(
+            nn.Linear(channels, channels, bias=True),
+            nn.SiLU(),
+            nn.Linear(channels, channels, bias=True),
+        )
+
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(channels, 3 * channels, bias=True)
+        )
+
+    def forward(self, x, y):
+        shift_mlp, scale_mlp, gate_mlp = self.adaLN_modulation(y).chunk(3, dim=-1)
+        h = modulate(self.in_ln(x), shift_mlp, scale_mlp)
+        h = self.mlp(h)
+        return x + gate_mlp * h
+
+class FinalLayer(nn.Module):
+    """
+    The final layer adopted from DiT.
+    """
+    def __init__(self, model_channels, out_channels):
+        super().__init__()
+        self.norm_final = nn.LayerNorm(model_channels, elementwise_affine=False, eps=1e-6)
+        self.linear = nn.Linear(model_channels, out_channels, bias=True)
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(model_channels, 2 * model_channels, bias=True)
+        )
+
+    def forward(self, x, c):
+        shift, scale = self.adaLN_modulation(c).chunk(2, dim=-1)
+        x = modulate(self.norm_final(x), shift, scale)
+        x = self.linear(x)
+        return x
+
+class SimpleMLPAdaLN(nn.Module):
+    """
+    The MLP for Diffusion Loss.
+    :param in_channels: channels in the input Tensor.
+    :param model_channels: base channel count for the model.
+    :param out_channels: channels in the output Tensor.
+    :param z_channels: channels in the condition.
+    :param num_res_blocks: number of residual blocks per downsample.
+    """
+
+    def __init__(
+        self,
+        in_channels,
+        model_channels,
+        out_channels,
+        z_channels,
+        num_res_blocks,
+        grad_checkpointing=False
+    ):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.model_channels = model_channels
+        self.out_channels = out_channels
+        self.num_res_blocks = num_res_blocks
+        self.grad_checkpointing = grad_checkpointing
+
+        self.time_embed = TimestepEmbedder(model_channels)
+        self.cond_embed = nn.Linear(z_channels, model_channels)
+
+        self.input_proj = nn.Linear(in_channels, model_channels)
+
+        res_blocks = []
+        for i in range(num_res_blocks):
+            res_blocks.append(ResBlock(
+                model_channels,
+            ))
+
+        self.res_blocks = nn.ModuleList(res_blocks)
+        self.final_layer = FinalLayer(model_channels, out_channels)
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        def _basic_init(module):
+            if isinstance(module, nn.Linear):
+                torch.nn.init.xavier_uniform_(module.weight)
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+        self.apply(_basic_init)
+
+        # Initialize timestep embedding MLP
+        nn.init.normal_(self.time_embed.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.time_embed.mlp[2].weight, std=0.02)
+
+        # Zero-out adaLN modulation layers
+        for block in self.res_blocks:
+            nn.init.constant_(block.adaLN_modulation[-1].weight, 0)
+            nn.init.constant_(block.adaLN_modulation[-1].bias, 0)
+
+        # Zero-out output layers
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].weight, 0)
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].bias, 0)
+        nn.init.constant_(self.final_layer.linear.weight, 0)
+        nn.init.constant_(self.final_layer.linear.bias, 0)
+
+    def forward(self, x, t, c):
+        """
+        Apply the model to an input batch.
+        :param x: an [N x C] Tensor of inputs.
+        :param t: a 1-D batch of timesteps.
+        :param c: conditioning from AR transformer.
+        :return: an [N x C] Tensor of outputs.
+        """
+        x = self.input_proj(x)
+        t = self.time_embed(t)
+        c = self.cond_embed(c)
+
+        y = t + c
+
+        if self.grad_checkpointing and not torch.jit.is_scripting():
+            for block in self.res_blocks:
+                x = checkpoint(block, x, y)
+        else:
+            for block in self.res_blocks:
+                x = block(x, y)
+
+        return self.final_layer(x, y)
+
+    def forward_with_cfg(self, x, t, c, cfg_scale):
+        half = x[: len(x) // 2]
+        combined = torch.cat([half, half], dim=0)
+        model_out = self.forward(combined, t, c)
+        eps, rest = model_out[:, :self.in_channels], model_out[:, self.in_channels:]
+        cond_eps, uncond_eps = torch.split(eps, len(eps) // 2, dim=0)
+        half_eps = uncond_eps + cfg_scale * (cond_eps - uncond_eps)
+        eps = torch.cat([half_eps, half_eps], dim=0)
+        return torch.cat([eps, rest], dim=1)
+
+@dataclass
+class ClassHeadCfg(CLIPTextCfg):
+    mlp_ratio: int = 4
+    layers: int = 1
+
+def _build_cls_head(
+        width,
+        clshead_cfg,
+        quick_gelu: bool = False,
+        cast_dtype: Optional[torch.dtype] = None,
+):
+    clshead_cfg = ClassHeadCfg(**clshead_cfg) if isinstance(clshead_cfg, dict) else clshead_cfg
+    act_layer = QuickGELU if quick_gelu else nn.GELU
+    norm_layer = (
+        LayerNormFp32 if cast_dtype in (torch.float16, torch.bfloat16) else LayerNorm
+    )
+
+    head = MixClsHead(
+        width=width,
+        layers=clshead_cfg.layers,
+        mlp_ratio=clshead_cfg.mlp_ratio,
+        act_layer=act_layer,
+        norm_layer=norm_layer,
+        output_dim=clshead_cfg.vocab_size,
+    )
+
+    return head
+
+class MultiTask(CLIP):
+    def __init__(self,
+                 ssl_mlp_dim: int,
+                 ssl_emb_dim: int,
+                 text_cfg: CLIPTextCfg,
+                 grad_checkpointing=False,
+                 diffloss_d=3,
+                 num_sampling_steps='100',
+                 decoder_embed_dim=512,
+                 quick_gelu: bool = False,
+                 cast_dtype: Optional[torch.dtype] = None,
+                 **kwargs,
+                 ):
+        super().__init__(**kwargs)
+
+        self.image_mlp = self._build_mlp(in_dim=self.vision_width, mlp_dim=ssl_mlp_dim, out_dim=ssl_emb_dim)
+        self.train_diffusion = create_diffusion(timestep_respacing="", noise_schedule="cosine")
+        self.token_embed_dim = 512
+        self.decoder = SimpleMLPAdaLN(
+            in_channels=self.token_embed_dim,
+            model_channels=self.vision_width,
+            out_channels=self.vision_width,
+            z_channels=decoder_embed_dim,
+            num_res_blocks=diffloss_d,
+            grad_checkpointing=grad_checkpointing
+        )
+        self.diffusion_batch_mul = 4
+        clshead_cfg = ClassHeadCfg(**text_cfg) if isinstance(text_cfg, dict) else text_cfg
+        self.text_decoder = _build_cls_head(
+            width=decoder_embed_dim,
+            clshead_cfg=clshead_cfg,
+            quick_gelu=quick_gelu,
+            cast_dtype=cast_dtype,
+        )
+        self.register_buffer("cap_fq", torch.zeros([1, self.vocab_size], dtype=torch.float64))
+        self.register_buffer("num_samples", torch.zeros([1, 1], dtype=torch.float64))
+
+    def _build_mlp(self, in_dim, mlp_dim, out_dim):
+        return nn.Sequential(OrderedDict([
+            ("layer1", nn.Linear(in_dim, mlp_dim)),
+            ("bn1", nn.SyncBatchNorm(mlp_dim)),
+            ("relu1", nn.ReLU(inplace=True)),
+            ("layer2", nn.Linear(mlp_dim, mlp_dim)),
+            ("bn2", nn.SyncBatchNorm(mlp_dim)),
+            ("relu2", nn.ReLU(inplace=True)),
+            ("layer3", nn.Linear(mlp_dim, out_dim)),
+        ]))
+
+    def patchify(self, x):
+        bsz, c, h, w = x.shape
+        p = 16
+        h_, w_ = h // p, w // p
+
+        x = x.reshape(bsz, c, h_, p, w_, p)
+        x = torch.einsum('nchpwq->nhwcpq', x)
+        x = x.reshape(bsz, h_ * w_, c * p ** 2)
+        return x  # [n, l, d]
+
+    def unpatchify(self, x, C, H, W):
+        bsz = x.shape[0]
+        p = 16
+        h, w = H // p, W // p
+
+        x = x.reshape(bsz, h, w, C, p, p)
+        x = torch.einsum('nhwcpq->nchpwq', x)
+        x = x.reshape(bsz, C, h * p, w * p)
+        return x  # [n, c, h, w]
+    
+    def encode(self, image):
+        x = self.visual(image)
+        x = x.last_hidden_state
+        x = x @ self.image_projection
+
+        return x
+
+    def forward(self, image, text):
+        B, C, H, W = image.shape
+        ## add noise
+        img = self.patchify(image)
+        noise = torch.randn_like(img)
+        ## 不同等级系数
+        alpha = torch.rand_like(img)
+        noise = noise * alpha
+        noise = self.unpatchify(noise, C, H, W)
+        t = torch.randint(0, self.train_diffusion.num_timesteps, (image.shape[0],), device=image.device)
+        x_t = self.train_diffusion.q_sample(image, t, noise)
+        ## diffusion
+        imgnoise_embed = self.encode(x_t) ## with noise
+        image_embed = self.encode(image) ## without noise
+        ## simclr
+        aug1_embed = self.image_mlp(self.visual(x_t).pooler_output)
+        aug2_embed = self.image_mlp(self.visual(image).pooler_output)
+        ## clip and superclass
+        img_embed_p = self.encode_image(image)
+        # imgnoise_embed_p = self.encode_image(x_t)
+        text_embed = self.encode_text(text)
+
+        ## add mar mlp as decoder, reshape操作参考mar code
+        B, L, _ = imgnoise_embed[:, 1:].shape
+        n_imgnoise_embed = imgnoise_embed[:, 1:].reshape(B * L, -1)
+        n_image_embed = image_embed[:, 1:].reshape(B * L, -1)
+        n_t = torch.randint(0, self.train_diffusion.num_timesteps, (n_image_embed.shape[0],), device=n_image_embed.device)
+        model_kwargs = dict(c=n_image_embed) ## clean feature token as condition
+        rec_noise = self.decoder(n_imgnoise_embed, n_t, **model_kwargs) ## recover noise
+        noise = self.patchify(noise)
+        B, L, _ = noise.shape
+        noise = noise.reshape(B * L, -1)
+
+        ## add img to text decode
+        text_tokens = self.text_decoder(img_embed_p)
+        labels = text.clone()
+
+        return {'aug1_embed': aug1_embed,
+                'aug2_embed': aug2_embed,
+                'img_embed_p': img_embed_p,
+                'text_embed': text_embed,
+                'noise': noise,
+                'rec_noise': rec_noise,
+                'text_tokens': text_tokens,
+                'labels': labels,
+                "cap_fq": self.cap_fq,
+                "num_samples": self.num_samples,
+                'logit_scale': self.logit_scale.exp()}
 
 # Define a new CLIP configuration
 config = CLIPConfig(
@@ -278,7 +630,10 @@ config = CLIPConfig(
     projection_dim=512      # Projection size for similarity calculation
 )
 
-def get_loss(model, ssl_temp, ssl_scale):
+def get_loss(model, ssl_temp, ssl_scale, diff_scale, cls_scale):
+    if model.startswith('MultiTask'):
+        ssl_loss = losses.SIMCLRLoss(temperature=ssl_temp)
+        return losses.MultiTaskLoss(ssl_loss, ssl_scale, diff_scale, cls_scale)
     if model.startswith('SLIP'):
         ssl_loss = losses.SIMCLRLoss(temperature=ssl_temp)
         return losses.SLIPLoss(ssl_loss, ssl_scale)
@@ -337,6 +692,14 @@ def CLIP_VITB16(**kwargs):
 
     return model
 
+def MultiTask_VITB16(**kwargs):
+    # vision_model = timm.create_model('vit_base_patch16_224', num_classes=0)
+    model_clip_transformer = CLIPModel(config)
+    vision_model = model_clip_transformer.vision_model
+    model = MultiTask(embed_dim=512, vision_width=768, vision_model=vision_model, context_length=77, vocab_size=49408,
+        transformer_width=512, transformer_heads=8, transformer_layers=12, **kwargs)
+
+    return model
 
 def SIMCLR_VITB16(**kwargs):
     vision_model = timm.create_model('vit_base_patch16_224', num_classes=0)
